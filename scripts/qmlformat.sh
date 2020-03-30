ecf5f03ebe784c81afe59a312a6521a1ffa091fc
Matthias Kuhn <matthias@opengis.ch>
2020-03-30 20:35:33 +0200

    Add qmlformat-wrapper.sh

--- scripts/qmlformat.sh
+++ scripts/qmlformat.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --rm -v $(pwd):/usr/src forderud/qtwasm:qt-5.15 /usr/src/scripts/ci/qmlformat.sh
+docker run -i --rm -v $(pwd):/usr/src forderud/qtwasm:qt-5.15 /usr/src/scripts/qmlformat-wrapper.sh $@
