8b987a390fdc45fee439fcd32ce01a0a67d53da9
Matthias Kuhn <matthias@opengis.ch>
2020-05-06 16:51:06 +0200

    Fixes

--- scripts/qmlformat.sh
+++ scripts/qmlformat.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -i --rm -v $(pwd):/usr/src forderud/qtwasm:qt-5.15 /usr/src/scripts/qmlformat-wrapper.sh $@
+docker run -i --rm -v $(pwd):/usr/src forderud/qtwasm:latest /usr/src/scripts/qmlformat-wrapper.sh $@
