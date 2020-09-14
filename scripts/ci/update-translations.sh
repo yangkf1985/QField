169b5bee064598b64294898c8f7a1550df683b8f
signedav <david@opengis.ch>
2020-09-14 23:38:14 +0200

    fixup

--- scripts/ci/update-translations.sh
+++ scripts/ci/update-translations.sh
@@ -5,7 +5,25 @@ source ${DIR}/../version_number.sh
 
 lupdate -recursive ${DIR}/../.. -ts ${DIR}/../../i18n/qfield_en.ts
 
+echo ==================0
+cat ${CI_BUILD_DIR}/i18n/qfield_en.ts
+echo ==================0
+
+echo ==================1
+grep Changelog ${CI_BUILD_DIR}/i18n/qfield_en.ts
+echo ==================1
+
+echo ==================2
+grep Changelog ${CI_BUILD_DIR}/i18n/qfield_bg.ts
+echo ==================2
+
+
 # release only if the branch is master
 if [[ ${CI_BRANCH} = master ]]; then
+  tx push --source
+fi
+
+# release only if the branch is master
+if [[ ${CI_BRANCH} = trans_test ]]; then
   tx push --source
 fi
