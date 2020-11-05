43215cf190fec91fd6667af14cd3b5eadf557f4b
signedav <david@opengis.ch>
2020-11-05 14:47:26 +0100

    morlocks

--- scripts/ci/pull_translations.sh
+++ scripts/ci/pull_translations.sh
@@ -2,7 +2,9 @@
 
 echo "::group::tx-pull"
 
+echo "i am here"
 if [[ ${CI_SECURE_ENV_VARS} = true ]]; then
+  echo "apparently not here"
   DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
   source ${DIR}/../version_number.sh
   
@@ -11,8 +13,10 @@ if [[ ${CI_SECURE_ENV_VARS} = true ]]; then
   for x in android/res/values-*_*;do mv $x $(echo $x | sed -e 's/_/-r/') ;done
   
   find ${DIR}/i18n -type f -name "*.ts" -exec lrelease "{}" \;
-  echo checking japanese grep -Ri -B1 -A2 "Show on map canvas" i18n/qfield_ja.ts
+  echo "another here"
+  echo "checking japanese grep -Ri -B1 -A2 "Show on map canvas" i18n/qfield_ja.ts"
   grep -Ri -B1 -A2 "Show on map canvas" ${DIR}/qfield_ja.ts
 fi
+echo "and out"
 
 echo "::endgroup::"
